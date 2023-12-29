eb2ceba3df5cea7cbb9e9b5bb61d34ba4c74bc53
AsrafulMasum <h.m.asrafulmasum@gmail.com>
2023-12-30 02:01:43 +0600

    random api & get by rank api created

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const port = process.env.PORT || 5000;
 
 app.use(
   cors({
-    origin: ["http://localhost:5173"],
+    origin: ["http://localhost:5173", "https://bigfinancetask.web.app"],
     credentials: true,
   })
 );
@@ -83,65 +83,141 @@ app.get("/", (req, res) => {
 });
 
 // users api method
-app.get("/users", async (req, res) => {
-  const cursor = usersCollections.find();
-  const result = await cursor.toArray();
-  res.send(result);
-});
-
 app.post("/users", async (req, res) => {
-  const user = req.body;
-  const email = user?.email;
-  const query = { email: email };
-  const isExists = await usersCollections.findOne(query);
-  if (isExists) {
-    res.send({ user: "Exist" });
-  } else {
-    const result = await usersCollections.insertOne(user);
-    res.send(result);
+  try {
+    const user = req.body;
+    const email = user?.email;
+    const query = { email: email };
+    const isExists = await usersCollections.findOne(query);
+    if (isExists) {
+      res.send({ user: "Exist" });
+    } else {
+      const result = await usersCollections.insertOne(user);
+      res.send(result);
+    }
+  } catch (error) {
+    console.error("Error posting users:", error);
+    res.status(500).send("Internal server error");
   }
 });
 
 // products api method
 app.post("/players", verifyCookie, async (req, res) => {
-  const playerInfo = req.body;
-  const result = await playersCollections.insertOne(playerInfo);
-  res.send(result);
+  try {
+    const playerInfo = req.body;
+    const result = await playersCollections.insertOne(playerInfo);
+    res.send(result);
+  } catch (error) {
+    console.error("Error posting players:", error);
+    res.status(500).send("Internal server error");
+  }
 });
 
 app.get("/players", async (req, res) => {
-  const cursor = playersCollections.find();
-  const result = await cursor.toArray();
-  res.send(result);
+  try {
+    const cursor = playersCollections.find();
+    const result = await cursor.toArray();
+    res.send(result);
+  } catch (error) {
+    console.error("Error fetching players:", error);
+    res.status(500).send("Internal server error");
+  }
 });
 
-app.get("/players/:id", async (req, res) => {
-  const id = req.params.id;
-  const query = { _id: new ObjectId(id) };
-  const result = await playersCollections.findOne(query);
-  res.send(result);
+app.get("/players/rank/:val", async (req, res) => {
+  try {
+    const rank = parseInt(req.params.val)-1;
+    const cursor = playersCollections.aggregate([
+      { 
+        $project: {
+          name: 1,
+          country: 1,
+          score: { $toInt: "$score" }
+        }
+      },
+      { $sort: { score: -1 } }
+    ]);
+
+    const result = await cursor.toArray();
+    const requestedRank = rank + 1;
+
+    if (requestedRank >= 1 && requestedRank <= result.length) {
+      const player = result[requestedRank - 1];
+      res.send(player);
+    } else {
+      res.status(404).send("Player not found");
+    }
+  } catch (error) {
+    console.error("Error fetching players:", error);
+    res.status(500).send("Internal server error");
+  }
 });
 
-app.put("/players/:id", async (req, res) => {
-  const id = req.params.id;
-  const filter = { _id: new ObjectId(id) };
-  const options = { upsert: true };
-  const updatePlayer = {
-    $set: {
-      name: req.body.name,
-      country: req.body.country,
-      score: req.body.score,
-    },
-  };
-  const result = await playersCollections.updateOne(filter,updatePlayer,options);
-  res.send(result);
+app.get("/players/random", async (req, res) => {
+  try {
+    const cursor = playersCollections.aggregate([
+      { $sample: { size: 1 } }
+    ]);
+
+    const result = await cursor.toArray();
+
+    if (result.length > 0) {
+      res.send(result[0]);
+    } else {
+      res.status(404).send("No players found");
+    }
+  } catch (error) {
+    console.error("Error fetching random player:", error);
+    res.status(500).send("Internal server error");
+  }
+});
+
+app.get("/players/:id", verifyCookie, async (req, res) => {
+  try {
+    const id = req.params.id;
+    const query = { _id: new ObjectId(id) };
+    const result = await playersCollections.findOne(query);
+    res.send(result);
+  } catch (error) {
+    console.error("Error fetching players:", error);
+    res.status(500).send("Internal server error");
+  }
+});
+
+app.put("/players/:id", verifyCookie, async (req, res) => {
+  try {
+    const id = req.params.id;
+    const filter = { _id: new ObjectId(id) };
+    const options = { upsert: true };
+    const updatePlayer = {
+      $set: {
+        name: req.body.name,
+        country: req.body.country,
+        score: req.body.score,
+      },
+    };
+    const result = await playersCollections.updateOne(
+      filter,
+      updatePlayer,
+      options
+    );
+    res.send(result);
+  } catch (error) {
+    console.error("Error updating players:", error);
+    res.status(500).send("Internal server error");
+  }
 });
 
 app.delete("/players/:id", verifyCookie, async (req, res) => {
-  const id = req.params.id;
-  const query = { _id: new ObjectId(id) };
-  const result = await playersCollections.deleteOne(query)
-  res.send(result);
+  try {
+    const id = req.params.id;
+    const query = { _id: new ObjectId(id) };
+    const result = await playersCollections.deleteOne(query);
+    res.send(result);
+  } catch (error) {
+    console.error("Error deleting players:", error);
+    res.status(500).send("Internal server error");
+  }
 });
 
 app.listen(port, () => {
